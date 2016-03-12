8285163d75cd6d28aa19b81b90f46ecd5ab561cd
Agam Agarwal <agammaster@gmail.com>
2016-03-12 12:15:16 +0530

    Read password from console instead of command line arguments

--- MailMerge.java
+++ MailMerge.java
@@ -18,19 +18,23 @@ import javax.mail.internet.MimeMessage;
 
 
 public class MailMerge {
-	
+
 	public static void main(String[] args) {
 
 		if(args.length<4) {
-			System.err.println("Usage: java MailMerge username password subject_file message_file list_file");
+			System.err.println("Usage: java MailMerge username subject_file message_file list_file");
 			System.exit(1);
 		}
 		final String username=args[0];
-		final String password=args[1];
+		String subjectFile = args[1];
+		String messageFile = args[2];
+		String listFile = args[3];
+
+		final String password = new String(System.console().readPassword("%s", "Password:"));
 
 		String subject=null;
 		try {
-			subject=new String(Files.readAllBytes(Paths.get(args[2])));
+			subject=new String(Files.readAllBytes(Paths.get(subjectFile)));
 		} catch(Exception e) {
 			System.err.println("Error in subject file");
 			System.exit(2);
@@ -38,7 +42,7 @@ public class MailMerge {
 
 		String message_text=null;
 		try {
-			message_text=new String(Files.readAllBytes(Paths.get(args[3])));
+			message_text=new String(Files.readAllBytes(Paths.get(messageFile)));
 		} catch(Exception e) {
 			System.err.println("Error in message file");
 			System.exit(2);
@@ -46,36 +50,36 @@ public class MailMerge {
 
 		BufferedReader br=null;
 		try {
-			br=new BufferedReader(new FileReader(args[4]));
+			br=new BufferedReader(new FileReader(listFile));
 		} catch(Exception e) {
 			System.err.println("Error in list file");
 			System.exit(3);
 		}
 
-		
+
 		Properties props=new Properties();
 		props.put("mail.smtp.auth", "true");
 		props.put("mail.smtp.starttls.enable", "true");
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
-		
+
 		Authenticator authenticator=new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
 				return new PasswordAuthentication(username, password);
 			}
 		};
-		
+
 		Session session=Session.getInstance(props, authenticator);
-		
+
 		String line;
 		try {
 			while((line=br.readLine())!=null) {
-				
+
 				String parts[]=line.split("\t");
 				String recipient=parts[0];
 				String message=constructMessage(message_text, parts);
-				
+
 				try {
 					Message msg=new MimeMessage(session);
 					msg.setFrom(new InternetAddress(username));
